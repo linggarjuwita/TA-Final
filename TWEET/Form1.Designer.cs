0d2fa4921c5ae6d6a12c6e6b9ec956125382ae50
LJH <real.linggarjh@yahoo.com>
2016-03-19 14:10:00 +0700

    tokenizing & filtering done!

---
+++ TWEET/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TWEET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(639, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tambah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(740, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Simpan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(28, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(787, 232);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(840, 401);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ TWEET/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TWEET
+{
+    public partial class Form1 : Form
+    {
+        DataTable dtTweet = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                OleDbConnection connExcel = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + openFileDialog1.FileName + ";Extended Properties='Excel 8.0;HDR=Yes'");
+                OleDbCommand cmdExcel = new OleDbCommand();
+                OleDbDataAdapter adtExcel = new OleDbDataAdapter();
+                cmdExcel.Connection = connExcel;
+
+                connExcel.Open();
+                DataTable schemaExcel = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                cmdExcel.CommandText = "SELECT tweet_id, name, gender, text From [" + schemaExcel.Rows[0]["TABLE_NAME"].ToString() + "]";
+                adtExcel.SelectCommand = cmdExcel;
+                adtExcel.Fill(dtTweet);
+                connExcel.Close();
+
+                textBox1.Text = openFileDialog1.FileName;
+                dataGridView1.DataSource = dtTweet;
+
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dtTweet.Rows.Count; i++)
+            {
+                int inDir = i + 1;
+                string text = dtTweet.Rows[i][3].ToString();
+                string cleanUp = Property.CleanUpTweets(text);
+                string tweet = Stopword.RemoveStopwords(cleanUp);
+                StreamWriter file = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "extraction\\" + inDir + ".txt");
+                file.WriteLine(tweet);
+                file.Close();
+            }
+
+            MessageBox.Show("Done!");
+        }
+    }
+}
